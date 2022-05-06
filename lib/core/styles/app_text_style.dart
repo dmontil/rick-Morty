e6304b68e7c31cb73b5d7a43f322197429a8789d
monti <dmontil96@gmail.com>
2022-05-07 00:02:58 +0200

    Add montserrat text styles

---
+++ lib/core/styles/app_text_style.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app_colors.dart';
+
+class AppTextStyle {
+  static final h1 = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 34,
+  );
+  static final h2 = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 28,
+  );
+  static final h3 = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w500,
+    fontSize: 22,
+  );
+  static final lightLarge = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w300,
+    fontSize: 18,
+  );
+  static final lightMedium = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w300,
+    fontSize: 16,
+  );
+  static final lightSmall = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w300,
+    fontSize: 14,
+  );
+
+  static final lightXS = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w300,
+    fontSize: 12,
+  );
+
+  static final regularLarge = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 18,
+  );
+  static final regularMedium = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+  static final regularSmall = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+
+  static final regularXS = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+
+  static final mediumLarge = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+  );
+  static final mediumMedium = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+  static final mediumSmall = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+
+  static final mediumXS = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+  );
+
+  static final semiBoldLarge = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+  );
+  static final semiBoldMedium = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+  static final semiBoldSmall = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w600,
+    fontSize: 14,
+  );
+
+  static final semiBoldXS = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w600,
+    fontSize: 12,
+  );
+
+  static final boldLarge = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 18,
+  );
+  static final boldMedium = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+  );
+  static final boldSmall = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+  );
+
+  static final boldXS = GoogleFonts.montserrat(
+    color: AppColors.black,
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+  );
+}
