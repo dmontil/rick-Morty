9e54833d1dab640ca713d3b3882261babcac557e
monti <dmontil96@gmail.com>
2022-05-07 00:59:46 +0200

    Add app spaces

---
+++ lib/core/styles/app_space.dart
@@ -0,0 +1,12 @@
+class AppSpaces {
+  static const double xxs2 = 2;
+  static const double xxs = 4;
+  static const double xs = 8;
+  static const double s = 12;
+  static const double m = 16;
+  static const double l = 20;
+  static const double xl = 24;
+  static const double xxl = 32;
+  static const double xxl2 = 40;
+  static const double xxl3 = 48;
+}
