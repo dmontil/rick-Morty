fa9b885b9092061e5a57e1c2db0fa24493226f83
monti <dmontil96@gmail.com>
2022-05-06 23:46:43 +0200

    Add colors

---
+++ lib/core/styles/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  static const c100 = Color(0xFF12555F);
+  static const c200 = Color(0xFFB9B9B9);
+  static const c300 = Color(0xFF7A7A7A);
+  static const c400 = Color(0xFF515151);
+  static const c500 = Color(0xFF081F32);
+  static const yellow = Color(0xFFEEBA00);
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+  static const green = Color(0xFFB2DF28);
+}
